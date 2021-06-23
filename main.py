a1affc4abda028789e5aa3cef2d4f4c6ef3b4391
Sieg79 <sieg@mail.ru>
2021-06-23 22:49:24 +0300

    all print added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.lect_rate:
@@ -18,12 +19,13 @@ class Student:
     def __str__(self):
         sep = '\n'
         summ_grade = 0
-        for grade in self.grades:
-            summ_grade += grade
+        for marks in list(self.grades.values()):
+            for mark in marks:
+                summ_grade += int(mark)
         if summ_grade == 0:
             ave_grade = 'оценок пока нет'
         else:
-            ave_grade = summ_grage / len(self.grades)
+            ave_grade = summ_grade / len(self.grades)
         courses_now = str(', '.join(self.courses_in_progress))
         courses_past = str(', '.join(self.finished_courses))
         description = f'Имя: {self.name}{sep}Фамилия: {self.surname}{sep}Средняя оценка за домашние задания: \
@@ -36,11 +38,24 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.lect_rate = {}
+
+    def __str__(self):
+        sep = '\n'
+        summ_grade = 0
+        for marks in list(self.lect_rate.values()):
+            for mark in marks:
+                summ_grade += int(mark)
+        if summ_grade == 0:
+            ave_grade = 'оценок пока нет'
+        else:
+            ave_grade = summ_grade / len(self.lect_rate)
+        description = f'Имя: {self.name}{sep}Фамилия: {self.surname}{sep}Средняя оценка за лекции: \
+{ave_grade}'
+        return description
 
 
 class Reviewer(Mentor):
@@ -55,7 +70,22 @@ class Reviewer(Mentor):
         else:
             print('Ошибка')
 
+    def __str__(self):
+        sep = '\n'
+        description = f'Имя: {self.name}{sep}Фамилия: {self.surname}'
+        return description
+
 cool_girl = Student('Ana', 'Nana', 'F')
 cool_girl.finished_courses += ['HTML']
 cool_girl.courses_in_progress += ['WEB', 'Target']
+clever_man = Reviewer('Bob', 'Bobson')
+clever_man.courses_attached += ['WEB', 'Target']
+clever_man.rate_hw(cool_girl, 'WEB', 10)
+clever_man.rate_hw(cool_girl, 'Target', 9)
+nice_guy = Lecturer('Peter', 'Peterson')
+nice_guy.courses_attached += ['Target', 'WEB']
+cool_girl.rate_lecturer(nice_guy, 'Target', 10)
+cool_girl.rate_lecturer(nice_guy, 'WEB', 8)
+print(clever_man)
 print(cool_girl)
+print(nice_guy)
